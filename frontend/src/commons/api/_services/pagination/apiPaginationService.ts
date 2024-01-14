bbec4179b06ab7f29c43555edb0306daf71e9085
Leonardo de Freitas Oliveira <leo_defoliveira@live.com>
2024-01-14 10:45:59 -0300

    frontend: update api pagination response

--- frontend/src/commons/api/_services/pagination/apiPaginationService.ts
+++ frontend/src/commons/api/_services/pagination/apiPaginationService.ts
@@ -3,36 +3,37 @@ import { PaginationParams } from "../../_types/pagination/paginationParams";
 import { PaginationParamsField } from "../../_types/pagination/paginationParamsField";
 
 export class ApiPaginationService {
-
-    static fromFetchParams<TField extends PaginationParamsField>(params: FetchParams): PaginationParams<TField> {
-        const { page, limit, sort, filter, select } = params;
-        return {
-          skip: ((page || 1) - 1) * limit,
-          ...(limit ? { take: limit } : {}),
-          ...(select ? { select } : {}),
-          ...(filter
-            ? {
-                where: {
-                  ...filter.reduce(
-                    (prev, curr) => ({
-                      ...prev,
-                      [curr.field]: { contains: curr.value },
-                    }),
-                    {}
-                  ),
-                },
-              }
-            : {}),
-          ...(sort
-            ? {
-                orderBy: {
-                  ...sort.reduce(
-                    (prev, curr) => ({ ...prev, [curr.field]: curr.order }),
-                    {}
-                  ),
-                },
-              }
-            : {}),
-        } as PaginationParams<TField>;
-      }
+  static fromFetchParams<TField extends PaginationParamsField>(
+    params: FetchParams
+  ): PaginationParams<TField> {
+    const { page, limit, sort, filter, select } = params;
+    return {
+      skip: ((page ?? 1) - 1) * (limit ?? 0),
+      ...(limit ? { take: limit } : {}),
+      ...(select ? { select } : {}),
+      ...(filter
+        ? {
+            where: {
+              ...filter.reduce(
+                (prev, curr) => ({
+                  ...prev,
+                  [curr.field]: { contains: curr.value },
+                }),
+                {}
+              ),
+            },
+          }
+        : {}),
+      ...(sort
+        ? {
+            orderBy: {
+              ...sort.reduce(
+                (prev, curr) => ({ ...prev, [curr.field]: curr.order }),
+                {}
+              ),
+            },
+          }
+        : {}),
+    } as PaginationParams<TField>;
+  }
 }
